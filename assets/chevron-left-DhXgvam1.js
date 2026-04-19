236845a2ec1ea53df25883629255aa97bbce3ee0
stb369 <35536182+stb369@users.noreply.github.com>
2026-04-19 14:29:13 +0900

    260419_01

---
+++ assets/chevron-left-DhXgvam1.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-D0WVbKfz.js";import"./index-B76M_X8m.js";import"./events-CHQwm8s6.js";import"./index.es-DklunbeP.js";import"./index-nibyPLVP.js";import"./node-B2DSZcqS.js";const m=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{m as chevronLeftSvg};
